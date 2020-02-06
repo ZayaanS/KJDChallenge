af4de4ca7b7de2a32c214424429147aad8d2aa03
Zayaan <zayaandots@gmail.com>
2020-02-06 14:17:17 +0200

    Updates

---
+++ precache-manifest.8c99e3a3b69f016e73dfcf730392c61b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ab16a9b329f89bbba9513b228890dd",
+    "url": "/KJDChallenge/index.html"
+  },
+  {
+    "revision": "8873bd157c6f6b1fb9a9",
+    "url": "/KJDChallenge/static/css/main.2c2fe084.chunk.css"
+  },
+  {
+    "revision": "648b3d2879586016c157",
+    "url": "/KJDChallenge/static/js/2.2878d028.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/KJDChallenge/static/js/2.2878d028.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8873bd157c6f6b1fb9a9",
+    "url": "/KJDChallenge/static/js/main.2e69f3da.chunk.js"
+  },
+  {
+    "revision": "7ce9a134383f5264e7ec",
+    "url": "/KJDChallenge/static/js/runtime-main.992d5b43.js"
+  },
+  {
+    "revision": "0e1b9872bdb0983d06e2061324b1e35e",
+    "url": "/KJDChallenge/static/media/sanlam.0e1b9872.png"
+  }
+]);
